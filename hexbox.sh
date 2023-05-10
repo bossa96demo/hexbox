72c62ce74204d2c5a2d6ccac6f89eb059a29720b
bossa96demo <ilyaershov09@gmail.com>
2023-05-10 21:07:04 +0300

    added support for passing argument from terminal

--- hexbox.sh
+++ hexbox.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Define a hex color code
-read -p "hex color(ex. #ab0b42): " color
+if [ $# -gt 0 ]; then
+	color=$1
+else
+	read -p "hex color(ex. ab0b42): " color
+fi
 
 # Convert the hex color code to decimal format
-r=$(printf '%d' "0x${color:1:2}")
-g=$(printf '%d' "0x${color:3:2}")
-b=$(printf '%d' "0x${color:5:2}")
+r=$(printf '%d' "0x${color:0:2}")
+g=$(printf '%d' "0x${color:2:2}")
+b=$(printf '%d' "0x${color:4:2}")
 
 # Use the ANSI escape code to change the text color
 echo -e "\e[38;2;${r};${g};${b}m███████\n███████\n███████\e[0m"
